b584b7a4cece15436e697133cc71b2b305fe873d
Sylsatra <136418580+Sylsatra@users.noreply.github.com>
2025-03-07 16:23:43 +0700

    Update FeathersWeaponryConfig.java
    Support Modded Items

--- src/main/java/com/example/feathers_weaponry/FeathersWeaponryConfig.java
+++ src/main/java/com/example/feathers_weaponry/FeathersWeaponryConfig.java
@@ -20,50 +20,45 @@ public class FeathersWeaponryConfig {
 
         builder.comment(
             "Time in ticks required for drawing a bow, crossbow, or trident before deducting feathers.\n" +
-            "Format: defineInRange(key, default, min, max)\n" +
-            "Default value: 15 ticks; Minimum: 1 tick; Maximum: 100 ticks.\n" +
-            "Increase this value if you want players to have to draw longer before feathers are deducted."
+            "Default value: 15 ticks; Minimum: 1 tick; Maximum: 100 ticks."
         );
         drawTimeThreshold = builder.defineInRange("drawTimeThreshold", 15, 1, 100);
 
         builder.comment(
             "Air (bubble) threshold. If the player's air is below this value, feather deductions are multiplied by 2.\n" +
-            "Format: defineInRange(key, default, min, max)\n" +
-            "Default value: 4; Minimum: 0; Maximum: 20.\n" +
-            "Set this value to control when low-air penalties kick in."
+            "Default value: 4; Minimum: 0; Maximum: 20."
         );
         bubbleThreshold = builder.defineInRange("bubbleThreshold", 4, 0, 20);
 
         builder.comment(
-            "List of valid item IDs for bow-like items. These items trigger the draw deduction when used.\n" +
-            "Examples: minecraft:bow, minecraft:crossbow, minecraft:trident.\n" +
-            "You can add or remove items as needed."
+            "List of valid item IDs for bow-like items. These items trigger the draw deduction when used."
         );
-        validBowItems = builder.define("validBowItems", Arrays.asList(
-            "minecraft:bow",
-            "minecraft:crossbow",
-            "minecraft:trident"
-        ));
+        validBowItems = builder.defineList("validBowItems", 
+            () -> Arrays.asList("minecraft:bow", "minecraft:crossbow", "minecraft:trident"),
+            object -> object instanceof String
+        );
 
         builder.comment(
-            "List of valid item IDs for weapon-based actions. These items trigger feather deductions when used to attack.\n" +
-            "You can include items for both main-hand and off-hand usage if desired."
+            "List of valid item IDs for weapon-based actions. These items trigger feather deductions when used to attack."
         );
-        validWeaponItems = builder.define("validWeaponItems", Arrays.asList(
-            "minecraft:iron_sword",
-            "minecraft:stone_sword",
-            "minecraft:wooden_sword",
-            "minecraft:golden_sword",
-            "minecraft:diamond_sword",
-            "minecraft:netherite_sword",
-            "minecraft:iron_axe",
-            "minecraft:stone_axe",
-            "minecraft:wooden_axe",
-            "minecraft:golden_axe",
-            "minecraft:diamond_axe",
-            "minecraft:netherite_axe",
-            "minecraft:trident"
-        ));
+        validWeaponItems = builder.defineList("validWeaponItems",
+            () -> Arrays.asList(
+                "minecraft:iron_sword",
+                "minecraft:stone_sword",
+                "minecraft:wooden_sword",
+                "minecraft:golden_sword",
+                "minecraft:diamond_sword",
+                "minecraft:netherite_sword",
+                "minecraft:iron_axe",
+                "minecraft:stone_axe",
+                "minecraft:wooden_axe",
+                "minecraft:golden_axe",
+                "minecraft:diamond_axe",
+                "minecraft:netherite_axe",
+                "minecraft:trident"
+            ),
+            object -> object instanceof String
+        );
 
         builder.pop();
         CONFIG = builder.build();
